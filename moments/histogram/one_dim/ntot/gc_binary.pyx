a0ff4326f87a56d66573892a8ab7f248a9fbc2ae
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2017-02-07 00:18:32 -0500

    Update gc_binary.pyx

--- moments/histogram/one_dim/ntot/gc_binary.pyx
+++ moments/histogram/one_dim/ntot/gc_binary.pyx
@@ -1,5 +1,6 @@
 """@docstring
+@brief Manipulate binary systems sampled according to the order parameter N_tot
 @author Nathan A. Mahynski
 @date 02/07/2017
-@brief Manipulate binary systems sampled according to the order parameter N_tot
+@filename gc_binary.pyx
 """
