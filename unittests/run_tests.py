9d178f14a4b8d1d0fa67ae4e5421fd186f96abf2
mahynski <nathan.mahynski@gmail.com>
2017-02-07 15:47:23 -0500

    removed ~ refernce to run_tests.py

--- unittests/run_tests.py
+++ unittests/run_tests.py
@@ -2,7 +2,7 @@ import os
 
 if __name__ == '__main__':
 	files = os.listdir('./')
-	tests = [f for f in files if '.py' in f and 'run_tests.py' != f and 'test.py' != f]
+	tests = [f for f in files if '.py' in f and 'run_tests.py' != f and 'test.py' != f and '~' not in f]
 	for test in tests:
 		print 14*'*****'+'\nRunning: '+test+'\n'+14*'*****'+'\n'
 		os.system('python '+test)
