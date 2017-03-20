d2901ce65b8578ba2137f6f1b210a7268baf71e7
mahynski <nathan.mahynski@gmail.com>
2017-03-20 10:04:51 -0400

    added compilation for checkpoint patching

--- moments/win_patch/setup.py
+++ moments/win_patch/setup.py
@@ -5,11 +5,17 @@ from numpy.distutils.core import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
-omcs_patch = Extension("omcs_patch", ["omcs_patch.pyx"], 
+omcs_patch = Extension("omcs_patch", ["omcs_patch.pyx"],
 	include_dirs=[np.get_include()],libraries=["m"],)
-	
-omcs_equil = Extension("omcs_equil", ["omcs_equil.pyx"], 
+
+omcs_equil = Extension("omcs_equil", ["omcs_equil.pyx"],
 	include_dirs=[np.get_include()],libraries=["m"],)
-	
-setup(cmdclass={'build_ext': build_ext}, 
-	ext_modules=[omcs_patch, omcs_equil])
+
+chkpt_equil = Extension("chkpt_equil", ["chkpt_equil.pyx"],
+	include_dirs=[np.get_include()],libraries=["m"],)
+
+chkpt_patch = Extension("chkpt_patch", ["chkpt_patch.pyx"],
+	include_dirs=[np.get_include()],libraries=["m"],)
+
+setup(cmdclass={'build_ext': build_ext},
+	ext_modules=[omcs_patch, omcs_equil, chkpt_equil, chkpt_patch])
