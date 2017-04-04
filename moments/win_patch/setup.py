c21f179a90f4f8df6603bde3454d902bb75db73d
mahynski <nathan.mahynski@gmail.com>
2017-04-04 12:35:12 -0400

    codefactor clean up

--- moments/win_patch/setup.py
+++ moments/win_patch/setup.py
@@ -6,16 +6,16 @@ from Cython.Distutils import build_ext
 import numpy as np
 
 fhmc_patch = Extension("fhmc_patch", ["fhmc_patch.pyx"],
-	include_dirs=[np.get_include()],libraries=["m"],)
+	include_dirs=[np.get_include()], libraries=["m"],)
 
 fhmc_equil = Extension("fhmc_equil", ["fhmc_equil.pyx"],
-	include_dirs=[np.get_include()],libraries=["m"],)
+	include_dirs=[np.get_include()], libraries=["m"],)
 
 chkpt_equil = Extension("chkpt_equil", ["chkpt_equil.pyx"],
-	include_dirs=[np.get_include()],libraries=["m"],)
+	include_dirs=[np.get_include()], libraries=["m"],)
 
 chkpt_patch = Extension("chkpt_patch", ["chkpt_patch.pyx"],
-	include_dirs=[np.get_include()],libraries=["m"],)
+	include_dirs=[np.get_include()], libraries=["m"],)
 
 setup(cmdclass={'build_ext': build_ext},
 	ext_modules=[fhmc_patch, fhmc_equil, chkpt_equil, chkpt_patch])
