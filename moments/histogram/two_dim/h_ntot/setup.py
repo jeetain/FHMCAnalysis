427a82af7e49dfcb983acc4e20cd53f0861b41a4
mahynski <nathan.mahynski@gmail.com>
2017-04-04 12:45:01 -0400

    codefactor clean up

--- moments/histogram/two_dim/h_ntot/setup.py
+++ moments/histogram/two_dim/h_ntot/setup.py
@@ -5,8 +5,8 @@ from numpy.distutils.core import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
-pore_hist = Extension("pore_hist", ["pore_hist.pyx"], include_dirs=[np.get_include()],libraries=["m"],)
-free_energy_profile = Extension("free_energy_profile", ["free_energy_profile.pyx"], include_dirs=[np.get_include()],libraries=["m"],)
-organize = Extension("organize", ["organize.pyx"], include_dirs=[np.get_include()],libraries=["m"],)
+pore_hist = Extension("pore_hist", ["pore_hist.pyx"], include_dirs=[np.get_include()], libraries=["m"],)
+free_energy_profile = Extension("free_energy_profile", ["free_energy_profile.pyx"], include_dirs=[np.get_include()], libraries=["m"],)
+organize = Extension("organize", ["organize.pyx"], include_dirs=[np.get_include()], libraries=["m"],)
 
 setup(cmdclass={'build_ext': build_ext}, ext_modules=[pore_hist, free_energy_profile, organize])
--- moments/histogram/two_dim/setup.py
+++ moments/histogram/two_dim/setup.py
@@ -5,5 +5,5 @@ from numpy.distutils.core import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
-joint_hist = Extension("joint_hist", ["joint_hist.pyx"], include_dirs=[np.get_include()],libraries=["m"],)
+joint_hist = Extension("joint_hist", ["joint_hist.pyx"], include_dirs=[np.get_include()], libraries=["m"],)
 setup(cmdclass={'build_ext': build_ext}, ext_modules=[joint_hist])
