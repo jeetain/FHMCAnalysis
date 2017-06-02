8d44bd5bbbd8cceefb595c18780ea9613ed21a93
mahynski <nathan.mahynski@gmail.com>
2017-06-02 11:15:47 -0400

    python updates

--- moments/histogram/one_dim/ntot/collect.py
+++ moments/histogram/one_dim/ntot/collect.py
@@ -12,22 +12,22 @@ def check_order_ (hist):
 	Check that the order alternates correctly.
 
 	Parameters
-        ----------
-        hist : histogram
-                Histogram that has been already segmented
+	----------
+	hist : histogram
+		Histogram that has been already segmented
 
 	"""
 
-        order = np.zeros(len(hist.data['ln(PI)_maxima_idx']) + len(hist.data['ln(PI)_minima_idx']))
-        if (hist.data['ln(PI)_maxima_idx'][0] < hist.data['ln(PI)_minima_idx'][0]):
-                order[::2] = hist.data['ln(PI)_maxima_idx']
-                order[1::2] = hist.data['ln(PI)_minima_idx']
-        else:
-                order[::2] = hist.data['ln(PI)_minima_idx']
-                order[1::2] = hist.data['ln(PI)_maxima_idx']
+	order = np.zeros(len(hist.data['ln(PI)_maxima_idx']) + len(hist.data['ln(PI)_minima_idx']))
+	if (hist.data['ln(PI)_maxima_idx'][0] < hist.data['ln(PI)_minima_idx'][0]):
+		order[::2] = hist.data['ln(PI)_maxima_idx']
+		order[1::2] = hist.data['ln(PI)_minima_idx']
+	else:
+		order[::2] = hist.data['ln(PI)_minima_idx']
+		order[1::2] = hist.data['ln(PI)_maxima_idx']
 
 	if (not (np.all([order[i] <= order[i+1] for i in xrange(len(order)-1)]))):
-                raise Exception ('Local maxima and minima not sorted correctly after collection')
+		raise Exception ('Local maxima and minima not sorted correctly after collection')
 
 def janus_collect (hist, **kwargs):
 	"""
@@ -41,11 +41,11 @@ def janus_collect (hist, **kwargs):
 	"""
 
 	# Check ln(PI) has been segmented first
-	if ('ln(PI)_maxima_idx' not in hist.data): 
+	if ('ln(PI)_maxima_idx' not in hist.data):
 		raise Exception('Histogram has not been segmented yet')
 	if ('ln(PI)_minima_idx' not in hist.data):
 		raise Exception('Histogram has not been segmented yet')
-	
+
 	# Also double check for correct order
 	check_order_ (hist)
 
@@ -61,7 +61,7 @@ def janus_collect (hist, **kwargs):
                         min_idx = [0]
 
 		last = hist.data['ln(PI)_minima_idx'][-1]
-		if (last > max_idx[0] and last < max_idx[1]):
+		if (max_idx[0] < last < max_idx[1]):
 			# Last minima in between maxima, so end of distribution is also a maxima
 			min_idx.append(last)
 		elif (last > max_idx[1]):
@@ -69,7 +69,7 @@ def janus_collect (hist, **kwargs):
 			assert (len(hist.data['ln(PI)_minima_idx']) > 1)
 			min_idx.append(hist.data['ln(PI)_minima_idx'][-2])
 			min_idx.append(hist.data['ln(PI)_minima_idx'][-1])
-		
+
 	# Check that new order alternates correctly
 	check_order_ (hist)
 
